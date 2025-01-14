bcf0df4bf5201318eef4b64feb92ad6eca9455df
MagdaIG <magda.inalaf@gmail.com>
2025-01-13 23:19:09 -0600

    Remove dist

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+const {resolve} = require("node:path");
 module.exports = defineConfig({
+  configureWebpack: {
+    performance: {
+      hints: false,
+      maxAssetSize: 1024000, // Aumenta el límite a 512 KiB
+      maxEntrypointSize: 1024000, // Aumenta el tamaño del punto de entrada
+    },
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias.set('@', resolve(__dirname, 'src'));
+  },
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
       ? '/Portafolio-vue/'
